65fab4d2da5bff03f69160bd102384d059a32ddd
daisuke <130036418+daisukess@users.noreply.github.com>
2024-05-21 00:48:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
 const display = document.querySelector('.display');
-const buttons = document.querySelectorAll('.number, .operator, .clear, .delete, .equal');
-const operators = ['%', '÷','×', '-', '+'];
+const buttons = document.querySelectorAll('button');
+const operators = ['%', '/', '*', '-', '+'];
 let output = '';
 
 const calculate = (value) => {
     if (value === '=' && output !== '') {
-        output = eval(output.replaceAll('%', '/100').replaceAll('×', '*').replaceAll('÷', '/')).toString();
+        try {
+            output = eval(output.replaceAll('%', '/100')).toString();
+        } catch {
+            output = 'Error';
+        }
     } else if (value === 'C') {
         output = '';
-        console.log('clear');
-    } else if (value === 'DEL'){
-        output = output.toString().slice(0, -1);
-    } else if (value === 'pm') {
-        if (operators.includes(output.slice(-1))) {
-            return;
-        }else {
-            output = output.slice(0, -1) + '(-' + output.slice(-1) + ')';
-        } 
-    }else if (value === '.') {
-        if (operators.includes(output.slice(-1)) || output.includes('.')) {
-            return;
-        }else {
-            output += value;
-        }   
-    }else {
-        if (output === '' && operators.includes(value)) {
-            return;
-        } else if (operators.includes(output.slice(-1)) && operators.includes(value)) {
-            output = output.slice(0, -1) + value;
-        } else {
+    } else if (value === 'DEL') {
+        output = output.slice(0, -1);
+    } else if (value === '±') {
+        if (output && !operators.includes(output.slice(-1))) {
+            output = (output[0] === '-' ? output.slice(1) : '-' + output);
+        }
+    } else if (value === '.') {
+        if (!output.includes('.')) {
             output += value;
         }
+    } else {
+        if (operators.includes(value) && (output === '' || operators.includes(output.slice(-1)))) {
+            return;
+        }
+        output += value;
     }
     display.value = output;
-    console.log(output);
 };
-
-const test = (value) => {
-    console.log(value);
-};
-
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => calculate(e.target.value));
